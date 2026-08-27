6564e792237d84dde01fb06d08aff102a435d65c
salmanbappi <salmanbappi@users.noreply.github.com>
2026-08-28 00:51:27 +0600

    fix(ui): Use Icons.Filled.HourglassEmpty for UPCOMING status

--- app/src/main/java/eu/kanade/presentation/anime/components/AnimeInfoHeader.kt
+++ app/src/main/java/eu/kanade/presentation/anime/components/AnimeInfoHeader.kt
@@ -695,7 +695,7 @@ private fun AnimeContentInfo(
                 SAnime.PUBLISHING_FINISHED.toLong() -> Icons.Outlined.Done
                 SAnime.CANCELLED.toLong() -> Icons.Outlined.Close
                 SAnime.ON_HIATUS.toLong() -> Icons.Outlined.Pause
-                SAnime.UPCOMING.toLong() -> Icons.Outlined.DateRange
+                SAnime.UPCOMING.toLong() -> Icons.Filled.HourglassEmpty
                 else -> Icons.Outlined.Block
             },
             text = when (status) {
