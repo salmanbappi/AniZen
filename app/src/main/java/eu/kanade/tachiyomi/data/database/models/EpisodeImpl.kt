71700f2e74ed6e07c2b1bfef9631e91c1625e4e5
salmanbappi <salmanbappi@users.noreply.github.com>
2026-08-30 23:34:15 +0600

    fix(app): implement memo on EpisodeImpl for SEpisode interface change

--- app/src/main/java/eu/kanade/tachiyomi/data/database/models/EpisodeImpl.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/database/models/EpisodeImpl.kt
@@ -2,6 +2,8 @@
 
 package eu.kanade.tachiyomi.data.database.models
 
+import kotlinx.serialization.json.JsonObject
+
 class EpisodeImpl : Episode {
 
     override var id: Long? = null
@@ -25,6 +27,8 @@ class EpisodeImpl : Episode {
     override var summary: String? = null
 
     override var preview_url: String? = null
+
+    override var memo: JsonObject = JsonObject.EMPTY
 
     override var last_second_seen: Long = 0
 
