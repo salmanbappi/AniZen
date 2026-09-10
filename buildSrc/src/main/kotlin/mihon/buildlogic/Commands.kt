599219bff0be31c60873a68d98c0a189ff6d140e
salmanbappi <salmanbappi@users.noreply.github.com>
2026-09-10 21:16:58 +0600

    chore: bump revision offset to r4600

--- buildSrc/src/main/kotlin/mihon/buildlogic/Commands.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/Commands.kt
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter
 // If it's not installed, you can return a random value as a workaround
 fun Project.getCommitCount(): String {
     val gitCount = runCommand("git rev-list --count HEAD").toIntOrNull() ?: 0
-    return (gitCount + 957).toString()
+    return (gitCount + 981).toString()
 }
 
 fun Project.getGitSha(): String {
